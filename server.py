d21773e41e8e62549fd98409f775e46cbf1daa55
Greg Osha <gregory.osha@gmail.com>
2022-11-29 17:55:41 -0500

    updated server file

--- server.py
+++ server.py
@@ -1,10 +1,32 @@
-import time
+from flask import Flask
+app = Flask(__name__)
+
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)   # use the BOARD pin-numbering system
-GPIO.setup(16, GPIO.IN)       # like pinMode(16, INPUT)
-while(1):                     # do this forever
-    if(GPIO.input(16)):       # like digitalRead(16)
-        print("Pin is high.")
-    else:
-        print("Pin is low.")
-    time.sleep(0.5)           # sleep for 0.5 s
+GPIO.setmode(GPIO.BOARD)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/pinon')
+def pin_on():
+    GPIO.setup(16, GPIO.OUT)   # pinMode(16, OUTPUT)
+    GPIO.output(16, GPIO.HIGH) # digitalWrite(16, HIGH)
+    return 'I turned on the pin.'
+
+# Below we take input from a web browser and channel it to GPIO pin.
+# app.route refers to your Pi's IP address, which you'll type into a web browser URL line when you want to control this code.
+# For example, to set pin 16 on your Pi to HIGH, in your web browser you'll type [your Pi IP address]/digital/write/16/HIGH.
+# Make sure the line below has the correct angle brackets in it.
+@app.route('/digital/write/<pin_name>/<state>')
+def digital_write(pin_name, state):
+    pin = int(pin_name)
+    if state.upper() in ['1', 'ON', 'HIGH']:
+        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        GPIO.output(pin, GPIO.HIGH) # digitalWrite(pin_name, HIGH)
+        return 'Set pin {0} to HIGH'.format(pin_name)
+    elif state.upper() in ['0', 'OFF', 'LOW']:
+        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        GPIO.output(pin, GPIO.LOW)  # digitalWrite(pin_name, LOW)
+        return 'Set pin {0} to LOW'.format(pin_name)
+    return 'Something went wrong'
