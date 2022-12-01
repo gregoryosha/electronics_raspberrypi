412359bbe018236e51c44644057c737210ff2a65
Greg Osha <gregory.osha@gmail.com>
2022-11-30 22:39:25 -0500

    adding javascript file

---
+++ templates/index.js
@@ -0,0 +1,8 @@
+fetch('/test')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(text){
+        console.log('GET response: ');
+        console.log(text.greeting);
+    })
