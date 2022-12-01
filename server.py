46d5ce6230295588130a0c0873442a2582dbaad8
Greg Osha <gregory.osha@gmail.com>
2022-11-30 22:32:47 -0500

    Added get and post requests

--- server.py
+++ server.py
@@ -8,3 +8,16 @@ GPIO.setmode(GPIO.BOARD)
 def home():
     example_embed='This string is from python'
     return render_template('index.html', embed=example_embed)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test_function():
+
+    #GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message) #Use JSON headers and serialize
+    
+    #POST request
+    if request.method == 'POST':
+        print(request.get_json()) #Parse as JSON
+        return 'Sucess', 200
