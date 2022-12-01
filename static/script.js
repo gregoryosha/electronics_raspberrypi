01f5780e780e6874473d568148fd26f65fe1b46d
Greg Osha <gregory.osha@gmail.com>
2022-12-01 16:03:01 -0500

    added keydown and keyup events

--- static/script.js
+++ static/script.js
@@ -3,12 +3,48 @@ function print_out() {
   alert("printing!");
 }
 
+function sendMessage(message){
+  $.get(`/digital/write/40/${message}`);
+}
+
 function led_on() {
   $.get("/digital/write/40/HIGH");
-  alert("LED! ON");
 }
 
 function led_off() {
   $.get("/digital/write/40/LOW");
-  alert("LED! ON");
 }
+
+
+
+window.addEventListener('keydown', (press)=>{
+  console.log(press)
+  if (press.key == 'w') {
+    sendMessage('HIGH');
+  }
+  // else if (press.key == 'a') {
+  //   sendMessage('left');
+  // }
+  // else if (press.key == 'd') {
+  //   sendMessage('right');
+  // }
+  // else if (press.key == 's') {
+  //   sendMessage('down');
+  // }
+})
+
+window.addEventListener('keyup', (press)=>{
+  if (press.key == 'w') {
+    sendMessage('LOW');
+  }
+  // }
+  // else if (press.key == 'a') {
+  //   sendMessage('stop');
+  // }
+  // else if (press.key == 'd') {
+  //   sendMessage('stop');
+  // }
+  // else if (press.key == 's') {
+  //   sendMessage('stop');
+  // }
+})
