bc42112d96bd919c8dbf3b086d4f1abf32c66f2f
ThatBenKraft <ben.kraft@rcn.com>
2022-12-01 18:39:24 -0500

    led test

--- controls.py
+++ controls.py
@@ -2,6 +2,15 @@ import time
 from enum import Enum, auto
 from typing import Any
 
-import RPi.GPIO
+import RPi.GPIO as GPIO
 
-# Test
+CHANNEL = 7
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(CHANNEL, GPIO.OUT)
+
+while True:
+    time.sleep(1)
+    GPIO.output(CHANNEL, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(CHANNEL, GPIO.LOW)
