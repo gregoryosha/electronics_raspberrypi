0082e9c61322022ff3834de34d47416645a02fe1
Greg Osha <gregory.osha@gmail.com>
2022-12-01 15:39:52 -0500

    added LED functionality

--- server.py
+++ server.py
@@ -15,15 +15,15 @@ def print_out():
     print("Successfuly get request!")
     return 'success!'
 
-# @app.route('/digital/write/<pin_name>/<state>')
-# def digital_write(pin_name, state):
-#     pin = int(pin_name)
-#     if state.upper() in ['1', 'ON', 'HIGH']:
-#         GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
-#         GPIO.output(pin, GPIO.HIGH) # digitalWrite(pin_name, HIGH)
-#         return 'Set pin {0} to HIGH'.format(pin_name)
-#     elif state.upper() in ['0', 'OFF', 'LOW']:
-#         GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
-#         GPIO.output(pin, GPIO.LOW)  # digitalWrite(pin_name, LOW)
-#         return 'Set pin {0} to LOW'.format(pin_name)
-#     return 'Something went wrong'
+@app.route('/digital/write/<pin_name>/<state>')
+def digital_write(pin_name, state):
+    pin = int(pin_name)
+    if state.upper() in ['1', 'ON', 'HIGH']:
+        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        GPIO.output(pin, GPIO.HIGH) # digitalWrite(pin_name, HIGH)
+        return 'Set pin {0} to HIGH'.format(pin_name)
+    elif state.upper() in ['0', 'OFF', 'LOW']:
+        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        GPIO.output(pin, GPIO.LOW)  # digitalWrite(pin_name, LOW)
+        return 'Set pin {0} to LOW'.format(pin_name)
+    return 'Something went wrong'
--- static/script.js
+++ static/script.js
@@ -2,3 +2,13 @@ function print_out() {
   $.get("/printout");
   alert("printing!");
 }
+
+function led_on() {
+  $.get("/digital/write/40/HIGH");
+  alert("LED! ON");
+}
+
+function led_off() {
+  $.get("/digital/write/40/LOW");
+  alert("LED! ON");
+}
