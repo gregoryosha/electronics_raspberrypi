b58798b44596861314d9978f82b11d5a74aa9728
Greg Osha <gregory.osha@gmail.com>
2022-12-04 16:59:04 -0500

    added pin declarations

--- server.py
+++ server.py
@@ -10,6 +10,8 @@ pin_states = {
     40: GPIO.LOW,
     38: GPIO.LOW
 }
+GPIO.setup(40, GPIO.OUT)
+GPIO.setup(38, GPIO.OUT)
 
 @app.route('/')
 def home():
@@ -24,12 +26,12 @@ def print_out():
 def digital_write(pin_name, state):
     pin = int(pin_name)
     if state.upper() in ['1', 'ON', 'HIGH']:
-        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        #GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
         #GPIO.output(pin, GPIO.HIGH) # digitalWrite(pin_name, HIGH)
         pin_states[pin] = GPIO.HIGH
         return 'Set pin {0} to HIGH'.format(pin_name)
     elif state.upper() in ['0', 'OFF', 'LOW']:
-        GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
+        #GPIO.setup(pin, GPIO.OUT)   # pinMode(pin_name, OUTPUT)
         #GPIO.output(pin, GPIO.LOW)  # digitalWrite(pin_name, LOW)
         pin_states[pin] = GPIO.LOW
         return 'Set pin {0} to LOW'.format(pin_name)
