dc25de84016fa7f7fbbab492d61a0a75cfecf198
ThatBenKraft <ben.kraft@rcn.com>
2022-12-04 17:15:05 -0500

    newtactic

--- controls.py
+++ controls.py
@@ -22,11 +22,35 @@ def main():
     #     GPIO.output(CHANNEL, False)  # type: ignore
 
     # Declare an named instance of class pass a name and type of motor
-    mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
-    time.sleep(0.5)
 
-    # call the function pass the arguments
-    mymotortest.motor_run(MOTOR_LEFT_PINS, 0.1, 50, False, True, "half", 0.05)
+    # mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
+    # time.sleep(0.5)
+
+    # # call the function pass the arguments
+    # mymotortest.motor_run(MOTOR_LEFT_PINS, 0.01, 10, False, False, "whole", 0.05)
+
+    for pin in MOTOR_LEFT_PINS:
+        GPIO.setup(pin, GPIO.OUT)  # type: ignore
+        GPIO.output(pin, 0)  # type: ignore
+
+    halfstep_seq = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1],
+    ]
+
+    for i in range(512):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(MOTOR_LEFT_PINS[pin], halfstep_seq[halfstep][pin])  # type: ignore
+                time.sleep(0.001)
+
+    GPIO.cleanup()  # type: ignore
 
 
 if __name__ == "__main__":
