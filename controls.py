40df376e9cbfe33f9e93f7300ea9026f6701e4c6
ThatBenKraft <ben.kraft@rcn.com>
2022-12-04 16:10:57 -0500

    newmotor

--- controls.py
+++ controls.py
@@ -3,33 +3,30 @@ from enum import Enum, auto
 from typing import Any
 
 import RPi.GPIO as GPIO
-import RpiMotorLib as stepper
+from RpiMotorLib import RpiMotorLib
 
 CHANNEL = 7
 
 GPIO.setmode(GPIO.BOARD)  # type: ignore
 GPIO.setup(CHANNEL, GPIO.OUT)  # type: ignore
 
-MOTOR_A_PINS = list(range(27, 35, 2))
-MOTOR_B_PINS = list(range(35, 39))
-
-
-# direction = 20  # Direction -> GPIO Pin
-# step = 21  # Step -> GPIO Pin
-
-# # Declare a instance of class pass GPIO pins numbers and the motor type
-# mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "DRV8825")
-
-# # call the function, pass the arguments
-# mymotortest.motor_go(False, "Full", 100, 0.01, False, 0.05)
+MOTOR_LEFT_PINS = list(range(27, 35, 2))
+MOTOR_RIGHT_PINS = list(range(35, 39))
 
 
 def main():
-    for i in range(5):
-        time.sleep(1)
-        GPIO.output(CHANNEL, True)  # type: ignore
-        time.sleep(1)
-        GPIO.output(CHANNEL, False)  # type: ignore
+    # for i in range(5):
+    #     time.sleep(1)
+    #     GPIO.output(CHANNEL, True)  # type: ignore
+    #     time.sleep(1)
+    #     GPIO.output(CHANNEL, False)  # type: ignore
+
+    # Declare an named instance of class pass a name and type of motor
+    mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
+    time.sleep(0.5)
+
+    # call the function pass the arguments
+    mymotortest.motor_run(MOTOR_LEFT_PINS, 0.1, 50, False, True, "half", 0.05)
 
 
 if __name__ == "__main__":
