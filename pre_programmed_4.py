5f9b99bae942e73f1c74f50903e59d4c132eaf97
ThatBenKraft <ben.kraft@rcn.com>
2022-12-06 11:54:33 -0500

    cleanup2

--- pre_programmed_4.py
+++ pre_programmed_4.py
@@ -1,7 +1,5 @@
 import time
 
-import RPi.GPIO as GPIO
-
 import controls
 
 time.sleep(1)
@@ -18,4 +16,4 @@ controls.move_forward(300)
 
 time.sleep(1)
 
-GPIO.cleanup()  # type: ignore
+controls.cleanup()
