31b6695bc0319a56ff911931174491d5cc30b533
Greg Osha <gregory.osha@gmail.com>
2022-12-06 01:40:52 -0500

    testing whole step

--- stepper_test.py
+++ stepper_test.py
@@ -7,16 +7,12 @@ for pin in control_pins:
   GPIO.output(pin, 0)
 halfstep_seq = [
   [1,0,0,0],
-  [1,1,0,0],
-  [0,1,0,0],
   [0,1,1,0],
-  [0,0,1,0],
   [0,0,1,1],
-  [0,0,0,1],
   [1,0,0,1]
 ]
 for i in range(3000):
-  for halfstep in range(8):
+  for halfstep in range(4):
     for pin in range(4):
       GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
     time.sleep(0.001)
