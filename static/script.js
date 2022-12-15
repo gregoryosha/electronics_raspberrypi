181a9ab3a4229e890e0b0c962040d2a10b5d5de4
Greg Osha <gregory.osha@gmail.com>
2022-12-15 14:52:41 -0500

    testing control pad

--- static/script.js
+++ static/script.js
@@ -61,3 +61,75 @@ window.addEventListener('keyup', (press) => {
   }
 })
 
+
+// document.body.addEventListener("click", function (e) {
+//   if (e.target && e.target.nodeName == "A") {
+//     e.preventDefault();
+//   }
+// });
+
+// function touchStartHandler(event) {
+//   var direction = event.target.dataset.direction;
+//   console.log('Touch Start :: ' + direction)
+//   sendMessage(direction);
+// }
+
+// function touchEndHandler(event) {
+//   const stop_command = 'stop';
+//   var direction = event.target.dataset.direction;
+//   console.log('Touch End :: ' + direction)
+//   sendMessage(stop_command);
+// }
+
+
+// document.querySelectorAll('.control').forEach(item => {
+//   item.addEventListener('touchstart', touchStartHandler);
+  
+// })
+
+// document.querySelectorAll('.control').forEach(item => {
+//   item.addEventListener('touchend', touchEndHandler)
+// })
+
+// Prevent scrolling on every click!
+
+// super sweet vanilla JS delegated event handling!
+document.body.addEventListener("click", function (e) {
+  if (e.target && e.target.nodeName == "A") {
+    e.preventDefault();
+  }
+});
+let dpads = Array.prototype.slice.call(
+    document.getElementsByClassName("d-pad"),
+    0
+  ),
+  opads = Array.prototype.slice.call(
+    document.getElementsByClassName("o-pad"),
+    0
+  ),
+  els = dpads.concat(opads);
+function dir(dir) {
+  for (let i = 0; i < els.length; i++) {
+    const el = els[i],
+      d = el.className.indexOf("d-") !== -1,
+      what = d ? "d-pad" : "o-pad";
+    console.log(what);
+    el.className = what + " " + dir;
+  }
+}
+document.body.onkeyup = function (e) {
+  switch (e.which) {
+    case 37:
+      dir("left");
+      break;
+    case 39:
+      dir("right");
+      break;
+    case 38:
+      dir("up");
+      break;
+    case 40:
+      dir("down");
+      break;
+  }
+};
