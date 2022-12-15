f3bb732f54c0a4291d5244d9882e74f2c8164c9a
Greg Osha <gregory.osha@gmail.com>
2022-12-15 15:31:24 -0500

    added Dpad direction information

--- static/script.js
+++ static/script.js
@@ -103,16 +103,14 @@ document.body.addEventListener("click", function (e) {
 function touchStartHandler(event) {
   var direction = event.target.dataset.direction;
   console.log('Touch Start :: ' + direction)
-  sendMessage(direction);
+  sendMessage(`${direction}/HIGH`);
 }
 
 function touchEndHandler(event) {
-  const stop_command = 'stop';
   var direction = event.target.dataset.direction;
   console.log('Touch End :: ' + direction)
-  sendMessage(stop_command);
+  sendMessage(`${direction}/LOW`);
 }
-
 
 document.querySelectorAll('.control').forEach(item => {
   item.addEventListener('touchstart', touchStartHandler);
@@ -123,37 +121,37 @@ document.querySelectorAll('.control').forEach(item => {
   item.addEventListener('touchend', touchEndHandler)
 })
 
-let dpads = Array.prototype.slice.call(
-    document.getElementsByClassName("d-pad"),
-    0
-  ),
-  opads = Array.prototype.slice.call(
-    document.getElementsByClassName("o-pad"),
-    0
-  ),
-  els = dpads.concat(opads);
-function dir(dir) {
-  for (let i = 0; i < els.length; i++) {
-    const el = els[i],
-      d = el.className.indexOf("d-") !== -1,
-      what = d ? "d-pad" : "o-pad";
-    console.log(what);
-    el.className = what + " " + dir;
-  }
-}
-document.body.onkeyup = function (e) {
-  switch (e.which) {
-    case 37:
-      dir("left");
-      break;
-    case 39:
-      dir("right");
-      break;
-    case 38:
-      dir("up");
-      break;
-    case 40:
-      dir("down");
-      break;
-  }
-};
+// let dpads = Array.prototype.slice.call(
+//     document.getElementsByClassName("d-pad"),
+//     0
+//   ),
+//   opads = Array.prototype.slice.call(
+//     document.getElementsByClassName("o-pad"),
+//     0
+//   ),
+//   els = dpads.concat(opads);
+// function dir(dir) {
+//   for (let i = 0; i < els.length; i++) {
+//     const el = els[i],
+//       d = el.className.indexOf("d-") !== -1,
+//       what = d ? "d-pad" : "o-pad";
+//     console.log(what);
+//     el.className = what + " " + dir;
+//   }
+// }
+// document.body.onkeyup = function (e) {
+//   switch (e.which) {
+//     case 37:
+//       dir("left");
+//       break;
+//     case 39:
+//       dir("right");
+//       break;
+//     case 38:
+//       dir("up");
+//       break;
+//     case 40:
+//       dir("down");
+//       break;
+//   }
+// };
